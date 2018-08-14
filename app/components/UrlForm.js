1d62ac470b933fcb959336861dc84d44f06be386
Glitch (react-starter-r16-w4) <none>
2018-08-14 22:48:51 +0000

    👽🍱 Checkpoint ./app/components/UrlForm.js:447779/1069 ./package.json:447779/2092 ./.babelrc:447779/51

--- app/components/UrlForm.js
+++ app/components/UrlForm.js
@@ -3,37 +3,37 @@ const React = require('react');
 const styled = require('styled-components');
 const { withFormik } = require('formik');
 
-const SubmitButton = styled.button`
-  display: inline-block;
-  font-weight: 400;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  border: 1px solid transparent;
-  padding: 0.5rem 0.75rem;
-  font-size: 1rem;
-  line-height: 1.25;
-  border-radius: 0.25rem;
-  transition: all 0.15s ease-in-out;
-  color: #007bff;
-  background-color: transparent;
-  background-image: none;
-  border-color: #007bff;
-  &:hover {
-    color: #fff;
-    background-color: #007bff;
-    border-color: #007bff;
-  }
-`;
+// const SubmitButton = styled.button`
+//   display: inline-block;
+//   font-weight: 400;
+//   text-align: center;
+//   white-space: nowrap;
+//   vertical-align: middle;
+//   -webkit-user-select: none;
+//   -moz-user-select: none;
+//   -ms-user-select: none;
+//   user-select: none;
+//   border: 1px solid transparent;
+//   padding: 0.5rem 0.75rem;
+//   font-size: 1rem;
+//   line-height: 1.25;
+//   border-radius: 0.25rem;
+//   transition: all 0.15s ease-in-out;
+//   color: #007bff;
+//   background-color: transparent;
+//   background-image: none;
+//   border-color: #007bff;
+//   &:hover {
+//     color: #fff;
+//     background-color: #007bff;
+//     border-color: #007bff;
+//   }
+// `;
 
-const ErrorLabel = styled.span`
-  display: inline-block;
-background-color: goldenrod;
-`;
+// const ErrorLabel = styled.span`
+//   display: inline-block;
+// background-color: goldenrod;
+// `;
 
 // Our inner form component which receives our form's state and updater methods as props
 const InnerForm = ({
@@ -59,16 +59,26 @@ Link Address
             onChange={handleChange}
             onBlur={handleBlur}
           />
-        </div>
-    {touched.url && errors.url && <div>{errors.url}</div>}
-
-          <div>
-            {values.hash}
           </div>
+    <input
+      type="email"
+      name="email"
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values.email}
+    />
+    {touched.email && errors.email && <div>{errors.email}</div>}
+    <input
+      type="password"
+      name="password"
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values.password}
+    />
+    {touched.password && errors.password && <div>{errors.password}</div>}
     <button type="submit" disabled={isSubmitting}>
       Submit
     </button>
-<SubmitButton/>
   </form>
 );
 
